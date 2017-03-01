4056fe28a27393bcc3d26895e0deb5b8d5b6e7eb
yuanyuanyuan <edwin@godblessyuan.com>
2017-03-01 10:53:23 +0800

    重新整理初始化测试目录

---
+++ test-webstorm/js/es6-module-test/b.js
@@ -0,0 +1,5 @@
+/**
+ * Created by jinhuiyuan on 17/2/28.
+ */
+
+export const users = ['root', 'admin', 'staff', 'ceo', 'chief', 'moderator'];
---
+++ test-webstorm/js/es6-module-test/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by jinhuiyuan on 17/2/28.
+ */
+export {db} from 'a';
+export {users} from 'b';
